18831f1cb20b811864cdcb3d5d9901ba50bb6b2b
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-14 13:22:21 +1300

    Removed wrapping div

--- src/Movie.js
+++ src/Movie.js
@@ -13,11 +13,6 @@ const POSTER_PATH = "http://image.tmdb.org/t/p/w154";
 
   */
 const Movie = ({ movie }) => (
-  <div>
-    <Link to={`/${movie.id}`}>
-      <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-    </Link>
-  </div>
 );
 
 export default Movie;
