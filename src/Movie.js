eb6d1bf15388856770d09c5b8f54ab7cbd923b68
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-14 13:22:37 +1300

    Added styled Poster component

--- src/Movie.js
+++ src/Movie.js
@@ -13,6 +13,9 @@ const POSTER_PATH = "http://image.tmdb.org/t/p/w154";
 
   */
 const Movie = ({ movie }) => (
+  <Link to={`/${movie.id}`}>
+    <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+  </Link>
 );
 
 export default Movie;
